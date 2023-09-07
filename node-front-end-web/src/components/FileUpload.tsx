5c4f4cb3827242a5cdc07dd50bc6e38cf906114f
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2023-09-07 16:51:15 -0300

    refactor(FileUpload): adding props to control change

--- node-front-end-web/src/components/FileUpload.tsx
+++ node-front-end-web/src/components/FileUpload.tsx
@@ -1,19 +1,38 @@
 import { FileInput, Label } from "flowbite-react";
+import { ChangeEvent } from "react";
 
 interface Props {
   helperText: string;
   id: string;
   name: string;
   label: string;
+  multiple: boolean;
+  accept: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export function FileUpload({ helperText, id, name, label }: Props) {
+export function FileUpload({
+  helperText,
+  id,
+  name,
+  label,
+  multiple,
+  accept,
+  onChange,
+}: Props) {
   return (
     <div className="max-w-md">
       <div className="mb-2 block">
         <Label htmlFor="file" value={label} />
       </div>
-      <FileInput helperText={helperText} id={id} name={name} />
+      <FileInput
+        helperText={helperText}
+        id={id}
+        name={name}
+        multiple={multiple}
+        accept={accept}
+        onChange={onChange}
+      />
     </div>
   );
 }

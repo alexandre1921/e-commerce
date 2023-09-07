4274f71931c329118a49102f819dd01410f49ee8
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2023-09-07 16:52:21 -0300

    feature(precification): uploading/reading data

--- node-front-end-web/src/App.tsx
+++ node-front-end-web/src/App.tsx
@@ -1,6 +1,30 @@
-import { FileUpload } from "./components";
+import { Button } from 'flowbite-react';
+import { ChangeEvent, useCallback, useRef, useState } from 'react';
+import { FileUpload } from './components';
+
+type FileReaderResultType = string | ArrayBuffer | null | undefined;
 
 function App() {
+  const currentFile = useRef<File | undefined>(undefined);
+  const [csvReadAsText, setCsvReadAsText] = useState<FileReaderResultType>(undefined);
+
+  const handleChangePrecificationFile = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files?.[0]) return;
+
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    currentFile.current = file;
+    reader.onload = ({ target }) => setCsvReadAsText(target?.result);
+    reader.readAsText(file);
+  }, []);
+
+  const handleClickValidate = useCallback(() => {
+    console.log('validating...', currentFile.current, csvReadAsText);
+  }, [csvReadAsText]);
+
+  const isValidateButtonDisabled = !csvReadAsText;
+
   return (
     <div>
       <FileUpload
@@ -8,7 +32,13 @@ function App() {
         label="Arquivo de precificação"
         id="precification_file"
         name="precification_file"
+        accept=".csv"
+        multiple={false}
+        onChange={handleChangePrecificationFile}
       />
+      <Button onClick={handleClickValidate} disabled={isValidateButtonDisabled}>
+        Validar
+      </Button>
     </div>
   );
 }
